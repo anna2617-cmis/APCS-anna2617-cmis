359f84b4c195867912cb2bca3255705971236f61
anna2617-APCS <anna2617@cmis.ac.th>
2018-08-09 14:31:37 +0700

    Finished Hello World

---
+++ FirstProject/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld //must have the same name 
+{ //ctrl+shit+I auto format 
+    public static void main (String[] args)//contain str 
+    //void specify what type of data java returns 
+    //main is the method name, first letter always lower case 
+    //main must contain (String[] args) 
+    //'()' is the arguement 
+    {
+        System.out.println("Hello World"); //Display the string 
+    } //end of method main 
+} //end of class 
+ 
