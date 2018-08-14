6c35c2e4f7995b26fc240d67c63dc24f4d37fefc
anna2617-APCS <anna2617@cmis.ac.th>
2018-08-14 09:34:01 +0700

    Started dataTypes

---
+++ FirstProject/Fundamentals_I.java
@@ -0,0 +1,19 @@
+public class Fundamentals_I
+{
+   public static void main (String[] args){
+       dataTypes();
+    }
+    
+   public static void dataTypes(){
+       byte a = 100;
+       short b = 10000;
+       int c = 100000;
+       long d = 100000L; 
+       float f1 = 234.5f; 
+       double d1 = 123.4;
+       boolean one = true; 
+       char letterA = 'A'; 
+       System.out.println("A byte is an 8-bit signed two's complement integer"); 
+       
+    }
+}
