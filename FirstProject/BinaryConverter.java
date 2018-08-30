9f0a7c01b885b7531b744878f8583c90df72c392
anna2617-cmis <anna2617@cmis.ac.th>
2018-08-30 10:50:47 +0700

    done with dec2bin

--- FirstProject/BinaryConverter.java
+++ FirstProject/BinaryConverter.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class BinaryConverter
 {
     public static void main(String args[]){
+        Scanner Scan = new Scanner (System.in);
         System.out.println("BinaryConverter");
+        int in_2 = Scan.nextInt(); 
+        System.out.println(dec2bin(in_2));
+
     }
     
     public static int bin2dec(String bin){
@@ -14,10 +19,20 @@ public class BinaryConverter
     }
     
     public static String dec2bin(int dec){
+        int pow = 0;
+        String output = "";
+        for (int a = 31; a >= 0; a--){
+            if (dec - Math.pow(2, a) >= 0 ){
+                output += 1;
+                dec -= Math.pow(2, a);
+            }else if  (dec - Math.pow(2, a) < 0 ){
+                output += 0;
+            }
+        }
         /*
          * Takes an int as an argument and returns a String representation
          * of the 32 bit binary.
          */
-        return "";
+        return output;
     }
 }
