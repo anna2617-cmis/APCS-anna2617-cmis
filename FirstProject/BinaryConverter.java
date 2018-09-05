9e42f3f1ee7157cb04d7ec252285b5f4bb5ad2b6
anna2617 <anna2617@cmis.ac.th>
2018-09-05 10:25:31 +0700

    bin2dec

--- FirstProject/BinaryConverter.java
+++ FirstProject/BinaryConverter.java
@@ -4,33 +4,52 @@ public class BinaryConverter
     public static void main(String args[]){
         Scanner Scan = new Scanner (System.in);
         System.out.println("BinaryConverter");
-        int in_2 = Scan.nextInt(); 
-        System.out.println(dec2bin(in_2));
+        int in_1 = Scan.nextInt(); 
+        System.out.println(dec2bin(in_1));
+        String in_2 = Scan.next(); 
+        System.out.println(bin2dec(in_2));
 
     }
 
     public static int bin2dec(String bin){
-        /*
-         * Takes a String that looks like a 32-bit binary number and returns
-         * the equivalent int value
-         */
+        int output = 0; 
+        if (bin.charAt(0) == '0'){
+            for (int a = 0; a < 32; a++){
 
-        return 0;
+                if (bin.charAt(a) == '1'){
+                    int b = 1; 
+                    output += Math.pow(2, b);
+                    b++; 
+                } else if (bin.charAt(a) == '0'){
+                    output += 0;
+                }
+
+            }
+
+        }else if (bin.charAt(0) == '1'){
+            int c = 0; 
+            String new_bin = "";
+            while (c < 32){
+                
+            }
+        }
+
+        return output;
     }
 
     public static String dec2bin(int dec){
-        int pow = 0;
         String output = "";
         if (dec < 0){
             dec = -dec;
             for (int a = 31; a >= 0; a--){
                 if (dec - Math.pow(2, a) >= 0 ){
-                    output += 0;
+                    output += "0";
                     dec -= Math.pow(2, a);
                 }else if  (dec - Math.pow(2, a) < 0 ){
-                    output += 1;
+                    output += "1";
                 }
             }
+
             if (dec % 2 == 0){
                 for (int b = output.length(); output.charAt(b-1)=='1'; b--){
                     output = output.substring(0,b-1)+"0"+output.substring(b);
@@ -39,15 +58,14 @@ public class BinaryConverter
                 int c = output.length();
                 while ( e == true){ 
                     int d = output.charAt(c-1); 
-                     
                     if (d =='1'){
-                        output = output.substring(0,c+1)+"1"+output.substring(c);
+                        output = output.substring(0,c)+"1"+output.substring(c+1);
                         e = false; 
                     }
                     c--;
                 }
             } else if (dec % 2 == 1){
-                output = output.substring(0,31)+"1"+output.substring(31);
+                output = output.substring(0,31)+"1"+output.substring(32);
             }
         }else {
             for (int a = 31; a >= 0; a--){
--- FirstProject/Git_names.java
+++ FirstProject/Git_names.java
@@ -2,8 +2,8 @@ public class Git_names
 {
     public static void main(String args[]){
         String new_out = "";
-        String output = "1011111";
-
+        String output = "11011101";
+        
         for (int b = output.length(); output.charAt(b-1)=='1'; b--){
             output = output.substring(0,b-1)+"0"+output.substring(b);
         }      
@@ -11,13 +11,12 @@ public class Git_names
         int c = output.length();
         while ( e == true){ 
             int d = output.charAt(c-1); 
-
             if (d=='1'){
                 output = output.substring(0,c)+"1"+output.substring(c+1);
                 e = false; 
             }
             c--; 
-            
+        
         }
         System.out.println(output); 
     }
