771a2a6d96d88f17eb37522778d635cbe16edb3e
anna2617 <anna2617@cmis.ac.th>
2018-09-06 13:34:19 +0700

    DONE

--- FirstProject/BinaryConverter.java
+++ FirstProject/BinaryConverter.java
@@ -17,18 +17,51 @@ public class BinaryConverter
             int b = 0;
             for (int a = 31; a >= 0; a--){
                 int c = (int)Math.pow(2, b);        
-                if (bin.charAt(a) == '1'){
-                    
+                if (bin.charAt(a) == '1'){                    
                     output += c;
-                    
                 }
                 b++; 
             }
 
         }else if (bin.charAt(0) == '1'){
-            
+            int a = 0; 
             String new_bin = "";
-
+            while (a < 32){
+                if (bin.charAt(a) == '1'){
+                    new_bin += "0"; 
+                }else{
+                    new_bin += "1";  
+                }
+                a++; 
+            } 
+            
+            if (new_bin.charAt(31)== '1'){
+                for (int b = new_bin.length(); new_bin.charAt(b-1)=='1'; b--){
+                    new_bin = new_bin.substring(0,b-1)+"0"+new_bin.substring(b);
+                }      
+                boolean e = true;
+                int c = new_bin.length();
+                while ( e == true){ 
+                    int d = new_bin.charAt(c-1); 
+                    if (d =='1'){
+                        new_bin = new_bin.substring(0,c)+"1"+new_bin.substring(c+1);
+                        e = false; 
+                    }
+                    c--;
+                }
+            } else if (new_bin.charAt(31)== '0'){
+                new_bin = new_bin.substring(0,31)+"1"+new_bin.substring(32);
+            }
+            
+            int b = 0;
+            for (int d = 31; d >= 0; d--){
+                int c = (int)Math.pow(2, b);        
+                if (new_bin.charAt(d) == '1'){                    
+                    output += c;
+                }
+                b++; 
+            }
+            output = - output;  
         }
 
         return output;
