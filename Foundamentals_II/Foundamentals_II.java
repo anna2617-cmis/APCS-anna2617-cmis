6c5b4eedc7aff990ced58cec5ecba615cba7cd41
anna <anna@cmis.ac.th>
2018-09-26 13:32:09 +0700

    ahh

--- Foundamentals_II/Foundamentals_II.java
+++ Foundamentals_II/Foundamentals_II.java
@@ -13,16 +13,110 @@ public class Foundamentals_II
     public static void printArray(int[] array, boolean skip){
         if(skip == false){
             for (int a = 0; a < array.length; a++){
-                System.out.print(array[a]); 
+                System.out.print(array[a] + " "); 
             }
-        }else if(skip == true){
+        }else 
             for (int a = 0; a < array.length; a++){
-                if (a%2==1){
-                    System.out.print(array[a]);
-                }
+
+                System.out.print(array[a] + " ");
+
             }
-        }
+
     }
-    
-    
+
+    public static void printArray(double[] array, boolean skip){
+        if(skip == false){
+            for (int a = 0; a < array.length; a++){
+                System.out.print(array[a] + " "); 
+            }
+        }else 
+            for (int a = 0; a < array.length; a++){
+
+                System.out.print(array[a] + " ");
+
+            }
+
+    }
+
+    public static void printArray(String[] array, boolean skip){
+        if(skip == false){
+            for (int a = 0; a < array.length; a++){
+                System.out.print(array[a] + " "); 
+            }
+        }else
+            for (int a = 0; a < array.length; a+=2){
+
+                System.out.print(array[a] + " ");
+
+            }
+
+    }
+
+    public static void printArray(boolean[] array, boolean skip){
+        if(skip == false){
+            for (int a = 0; a < array.length; a++){
+                System.out.print(array[a] + " "); 
+            }
+        }else
+            for (int a = 0; a < array.length; a+=2){
+
+                System.out.print(array[a] + " ");
+
+            }
+
+    }
+
+    public static int lastItem(int[] array){
+        int output = array[array.length - 1]; 
+        return output; 
+    }
+
+    public static double lastItem(double[] array){
+        double output = array[array.length - 1]; 
+        return output; 
+    }
+
+    public static String lastItem(String[] array){
+        String output = array[array.length - 1]; 
+        return output; 
+    }
+
+    public static boolean lastItem(boolean[] array){
+        boolean output = array[array.length - 1]; 
+        return output; 
+    }
+
+    public static int middleItem(int[] array){
+        int mid = (array.length -1) / 2 ;
+        int output = array[mid];
+        return output; 
+    }
+
+    public static double middleItem(double[] array){
+        int mid = (array.length -1) / 2 ;
+        double output = array[mid];
+        return output; 
+    }
+
+    public static String middleItem(String[] array){
+        int mid = (array.length -1) / 2 ;
+        String output = array[mid];
+        return output; 
+    }
+
+    public static boolean middleItem(boolean[] array){
+        int mid = (array.length -1) / 2 ;
+        boolean output = array[mid];
+        return output; 
+    }
+
+    public static int[] randomInts(int n, int min, int max){
+        int[] output = new int[n];
+        for (int a = 0; a < n; a++){
+            output[a] = (int) (Math.random * max) -min; 
+        }
+        
+        return output; 
+    }
+
 }
