2428ddc3d3a198e486bace533cc4873ddc232068
anna2617-APCS <anna2617@cmis.ac.th>
2018-10-01 03:17:50 +0700

    w

--- Foundamentals_II/Foundamentals_II.java
+++ Foundamentals_II/Foundamentals_II.java
@@ -7,7 +7,9 @@ public class Foundamentals_II
         double[] ar_2 = {2.3, 3.3, 12.2, 2.4, 5.3}; 
         String[] ar_3 = {"a", "s", "d", "f", "g"}; 
         System.out.format("The first integer is %d, the third double is %f, the fifth string is %s.", ar_1[0], ar_2[2], ar_3[4]); 
+        
     }
+    
 
     public static void printArray(int[] array, boolean skip){
         if(skip == false){
@@ -166,11 +168,20 @@ public class Foundamentals_II
     
     public static int[] concat(int[] a, int[] b){
         int[] output = new int[a.length + b.length];
-        for (int i = 0; i < a.length + b.length -1; i++){
-        
+        int c = 0; 
+        for (int i = 0; i < a.length; i++){
+            output[i] = a [i];
         } 
-        
+        for (int x = a.length; x < a.length + b.length; x++){
+            output[x] = b [c];
+            c++; 
+        }
         return output; 
     }
     
+    public static int[] merge(int[] a, int[] b){
+        int[] output = new int[a.length + b.length];
+        
+        return output; 
+    }
 }
