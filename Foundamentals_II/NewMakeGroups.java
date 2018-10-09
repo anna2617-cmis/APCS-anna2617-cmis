b14d1096e0d5025b0c4ae37e0c29e330a86a0c81
Anna <anna2617-cmis>
2018-10-09 14:36:31 +0700

    Groups

--- Foundamentals_II/NewMakeGroups.java
+++ Foundamentals_II/NewMakeGroups.java
@@ -1,15 +1,31 @@
 public class NewMakeGroups
 {
-    public static void makeGroups(String[] names, int groupsize){
+    public static String[] makeGroups(String[] names, int groupsize){        
         String[] output = new String[names.length / groupsize]; 
-        int random = (int)(Math.random() * (names.length));
-        System.out.print(random); 
-        for (int a = 0; a < names.length; a++){
-            if (a == random){
-                output[0] = names[a]; 
+        String[] array = new String[names.length]; 
+        for(int a = 0; a<names.length; a++){
+            int random = (int)(Math.random() * (names.length));
+            if (array[random] == null){
+                array[random] = names[a]; 
+            }else{
+                while(array[random]!= null){
+                    random = (int)(Math.random() * (names.length));
+                }
+                array[random] = names[a]; 
             }
-
         }
-
+        int j = 0; 
+        for(int b = 0; b < array.length; b++){ 
+            if (j == output.length){
+                j = 0; 
+            }
+            if(output[j] == null){
+                output[j] = array[b]; 
+            }else{
+                output[j] += array[b]; 
+            }
+            j++;
+        }
+        return output; 
     }
 }
