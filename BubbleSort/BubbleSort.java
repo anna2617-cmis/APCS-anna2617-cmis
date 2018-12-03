40852885f45b15bf72521092e7b43ccf7d318846
anna <anna2617@cmis.ac.th>
2018-12-03 14:39:22 +0700

    a

---
+++ BubbleSort/BubbleSort.java
@@ -0,0 +1,54 @@
+public class BubbleSort
+{
+    public static void main(String[] args){
+        int[] array = getArray(20, true);
+        print(array);
+        sort(array);
+        print(array);
+    }
+
+    public static int[] sort(int[] array){
+        boolean sort = true; 
+        int small = 0; 
+        int big = 0; 
+        while (sort){
+            int swap = 0; 
+            for (int a = 0; a < array.length - 1; a++){
+                if (array[a] < array[a+1]){
+                    small = array[a+1]; 
+                    big = array[a]; 
+                    array[a+1] = small; 
+                    array[a] = big; 
+                    swap += 1; 
+                }
+            }
+            if (swap == 0){
+                sort = false; 
+            }
+        }
+        return array; 
+    }
+
+    public static int[] getArray(int w, boolean random){
+        int[] array = new int[w];
+        int ct = 0;
+        for(int i = 0; i < w; i++){
+            if(random)
+                array[i] = (int)(Math.random() * 20000) * (Math.random() > 0.4 ? -1 : 1);
+            else
+                array[i] = ct++;
+        }
+        return array;
+    }
+
+    public static void print(int[] array){
+        String out = "{";
+        for(int i = 0; i < array.length; i++){
+            out += array[i];
+            if( i != array.length -1)
+                out+=", ";
+        }
+        out+= "}\n";
+        System.out.println(out);
+    }
+}
