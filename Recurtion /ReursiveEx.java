6bd30426214d5251058da541636fe0effd7e0fec
anna <anna2617@cmis.ac.th>
2019-01-16 11:58:42 +0700

    rere

---
+++ Recurtion /ReursiveEx.java
@@ -0,0 +1,22 @@
+public class ReursiveEx
+{
+    public static int mul(int a, int b){
+        if (b == 1){
+            return a; 
+        }else if (b == 0 || a == 0){
+            return 0; 
+        }else{
+            return a + mul(a, b-1); 
+        }
+    }
+
+    public static int div(int a, int b){
+        if (b == 1){
+            return a; 
+        }else if (a == 0){
+            return 0; 
+        }else{
+            return 1 + div(a - b, b) ; 
+        }
+    }
+}
