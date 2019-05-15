ceafefba448bb4eca8c2f239c2c5a7c304fc6902
a <a>
2019-05-15 09:20:56 +0700

    a

--- test/Devil.java
+++ test/Devil.java
@@ -2,25 +2,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Devil extends Actor
 {
-    
+
     public void act() 
     {
-        int x = getX(); 
-        int y = getY(); 
         int bx = ((MyWorld)getWorld()).getBird().getX(); 
         int by = ((MyWorld)getWorld()).getBird().getY(); 
         double ranodm = Math.random(); 
-        if (bx > x){
-            x++; 
-        }else{
-            x--; 
+        height = 25; 
+        width = 25; 
+        turnTowards(bx,by); 
+        move(1); 
+        if(getX() == bx && getY() == by){
+            image.scale(width, hight); 
         }
-        
-        if (by > y){
-            y++; 
-        }else{
-            y--; 
-        }
-        move(3); 
-    }    
+    }
 }
